dd630cae64ce845abcecd36ad6b6fad2ee463221
Valerie Bragg <vbragg@ufledu>
2019-06-15 00:10:05 -0400

    Add JS to main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 var listItems = document.querySelectorAll('#buttons li');
 
 function changeColor() {
-  this.classList.toggle('blue');
-}
+  this.classList.add('blue');
+  this.removeAttribute('id');
+};
 
 for (i = 0; i< listItems.length; i++) {
   listItems[i].addEventListener('mouseover', changeColor);
-}
+};
+
+
+var firstButton = document.getElementById('button1');
+
+
+firstButton.addEventListener('click',
+  function() {
+      alert('Oooh - so close, but no cigar');
+  },
+);
+
+var secondButton = document.getElementById('button2');
+
+
+secondButton.addEventListener('click',
+  function() {
+      alert('DING DING DING - We have a winner');
+  },
+);
+
+var thirdButton = document.getElementById('button3');
+
+
+thirdButton.addEventListener('click',
+  function() {
+      alert('Oops, butter luck next time”');
+  },
+);
